0d3ac1c6fa12d75adfa91dc6389a0aee718c2b4a
Cristian <cj83774932@gmail.com>
2025-05-27 18:05:01 -0600

    Resolve conflicts with sweetalert2 modals

--- resources/views/citizens/index.blade.php
+++ resources/views/citizens/index.blade.php
@@ -90,26 +90,4 @@
             </div>
         </div>
     </div>
-    @if(session('success'))
-    @push('scripts')
-    <script>
-        const Toast = Swal.mixin({
-        toast: true,
-        position: "top-end",
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-            toast.addEventListener('mouseenter', Swal.stopTimer)
-            toast.addEventListener('mouseleave', Swal.resumeTimer)
-        }
-        });
-
-        Toast.fire({
-        icon: "success",
-        title: "{{ session('success') }}"
-        });
-    </script>
-    @endpush
-    @endif
 </x-app-layout>
